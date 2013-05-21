cc53cf14b4b430fe3ef97a14ab8ae473ff2348b6
David Wilkie <dwilkie@gmail.com>
2013-05-21 18:26:08 +0700

    don't destructively modify configuration hash

--- lib/smpp_client/core_ext/hash.rb
+++ lib/smpp_client/core_ext/hash.rb
@@ -1,8 +1,5 @@
 class Hash
-  def symbolize_keys!
-    keys.each do |key|
-      self[(key.to_sym rescue key) || key] = delete(key)
-    end
-    self
+  def symbolize_keys
+    Hash[self.map{|(k,v)| [k.to_sym,v]}]
   end
 end
--- spec/smpp_client/core_ext/hash_spec.rb
+++ spec/smpp_client/core_ext/hash_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
+require 'smpp_client/core_ext/hash'
 
 describe Hash do
-  describe "#symbolize_keys!" do
-    it "should destructively symbolize the keys of the hash" do
+  describe "#symbolize_keys" do
+    it "should return a new hash with the keys symbolized" do
       hash = {"foo" => "bar", :bar => "foo"}
-      hash.symbolize_keys!
-      hash.should == {:foo => "bar", :bar => "foo" }
+      hash.symbolize_keys.should == {:foo => "bar", :bar => "foo" }
     end
   end
 end
